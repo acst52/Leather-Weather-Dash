e2576351ffba23e661b00c20822d4d9b8f3e9d5a
Ash <acst52@gmail.com>
2023-01-20 12:47:26 -0500

    added pseudocode and fixed vars

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,25 @@
 // To get the latitude and longitude of a city that the user inputs, 
 // we use the fetch method to call the OpenCage Geocoding API:
 
+// Let's start with some global vars:
+const APIKey1 = "a03e4beee32f480681623329a1fb8030";  // can use same API for both*
+const APIKey2 = "d460b3121f81ff3c2ab30beee768e22b";
+const city = document.getElementById("cityInput").value.trim();
 const todayContainer = document.getElementById("response")
 const form = document.getElementById("weatherForm");
+
+// Let's define the city coordinates globally to use in different fetchez
+let lat;
+let lon;
+
+// Once user submits a city, let's:
+    // 1. prevent page from refreshing
+    // 2. make sure the city field isn't empty
+    // 3. feed city into OpenCageData API to get lat & lon coords
+    // 4. use lat & lon in OpenWeatherMap to get current weather
+    // 5. generate HTML elements and display weather data to user
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    const APIKey1 = "a03e4beee32f480681623329a1fb8030";  // can use same API for both*
-    const APIKey2 = "d460b3121f81ff3c2ab30beee768e22b";
-    const city = document.getElementById("cityInput").value.trim();
     if (city !== "") {  // as long as city isn't empty,
         // Make a call to the OpenCage Geocoding API to get the latitude and longitude
         fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${APIKey1}`)
@@ -16,8 +28,8 @@ form.addEventListener("submit", function(event) {
             console.log(data);
             // Get the latitude and longitude from the OpenCageData API response, make sure arrays are not empty:
         if (data.results[0] !== undefined && data.results[0].geometry !== undefined) {
-            const lat = data.results[0].geometry.lat;
-            const lon = data.results[0].geometry.lng;
+            lat = data.results[0].geometry.lat;
+            lon = data.results[0].geometry.lng;
             
             // Now that we have the lat & longitude, we can make a request to the OpenWeatherMap API using fetch:
             fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey2}`)
@@ -72,3 +84,6 @@ form.addEventListener("submit", function(event) {
         console.error("Error fetching geolocation data: City name cannot be empty")
     };
 });
+
+console.log(lat);
+console.log(lon);
